421b4be02efe0041b19baf9bd3de5d64bfa3358a
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2014-01-10 19:13:47 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactApp::Application.config.secret_key_base = '6329f65e6a531d5c5ca5506e8d058344cb37d5dd6b22220bf4190629c2ef51e367f9041bfb90669992e0fa0b7d76067b65194f3be72c6df3a628733fb11e47ec'
